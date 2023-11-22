c27075f50f067489ad33ff2bece256f78020f717
Martin Hennecke <43222297+tox1c90@users.noreply.github.com>
2023-11-22 16:17:20 +0100

    Update and support for powermax and calculated statistics from ring buffer.

--- CoherentPEM.py
+++ CoherentPEM.py
@@ -7,9 +7,10 @@
 # Author: Martin Hennecke / Email: hennecke@mbi-berlin.de
 
 from enum import IntEnum
-from tango import AttrWriteType, DevState, DebugIt, DispLevel, Attr, READ_WRITE, CmdArgType
+from tango import AttrWriteType, DevState, DebugIt, DispLevel, Attr, READ_WRITE, CmdArgType, AttributeProxy
 from tango.server import Device, attribute, command, device_property
 import serial, random, time
+import numpy as np
 
 class CoherentPEM(Device):
     '''
@@ -27,6 +28,16 @@ class CoherentPEM(Device):
                          dtype='DevString',
                          access=AttrWriteType.READ,
                          doc='Device ID string')
+    
+    ModelName = attribute(label='Model',
+                         dtype='DevString',
+                         access=AttrWriteType.READ,
+                         doc='Model name of the device')
+    
+    SerialNumber = attribute(label='Serial',
+                         dtype='DevString',
+                         access=AttrWriteType.READ,
+                         doc='Serial number of the device')
     
     Value = attribute(label='Value',
                          dtype='DevDouble',
@@ -62,6 +73,12 @@ class CoherentPEM(Device):
                          access=AttrWriteType.READ_WRITE,
                          doc='Sets/queries the gain compensation factor')
 
+    Polling = attribute(label='Polling',
+                         dtype='DevLong',
+                         unit='ms',
+                         access=AttrWriteType.READ_WRITE,
+                         doc='Set/queries the polling interval of the power meter')
+
 # ------ default functions that are inherited from parent "Device" ------ #
     def init_device(self):
         Device.init_device(self)
@@ -74,6 +91,10 @@ class CoherentPEM(Device):
             self.ser.flushOutput()
             self.ser.write(bytearray("*IDN?\r",'ascii'))
             self.ID = self.ser.readline().decode("utf-8").lstrip().rstrip()
+            self.ser.write(bytearray("SYSTem:INFormation:SNUMber?\r",'ascii'))
+            self.SN = self.ser.readline().decode("utf-8").lstrip().rstrip().strip('\"')
+            self.ser.write(bytearray("SYSTem:INFormation:MODel?\r",'ascii'))
+            self.Model = self.ser.readline().decode("utf-8").lstrip().rstrip().strip('\"')
             self.info_stream("Connected to device ID: {:s}".format(self.ID))
 
             if 'EnergyMax' in self.ID:
@@ -85,6 +106,12 @@ class CoherentPEM(Device):
                     self.statmode = True
                 else:
                     self.statmode = False
+            
+            if 'PowerMax' in self.ID:
+                self.ser.write(bytearray("CONFigure:ITEMselect MEAS,FLAG,TST\r",'ascii'))
+
+            self.value_attr = AttributeProxy(self.get_name()+"/value")
+            self.poll_buffer = self.get_attr_poll_ring_depth("Value")
 
             self.set_status("The device is in ON state")
             self.set_state(DevState.ON)
@@ -95,7 +122,8 @@ class CoherentPEM(Device):
 
     def initialize_dynamic_attributes(self):
         if 'EnergyMax' in self.ID:
-            Statistics_mode = attribute(name='Statistics Mode',
+            Statistics_mode = attribute(name='Statistics_mode',
+                            label='Statistics Mode',
                             dtype='DevBoolean',
                             access=AttrWriteType.READ_WRITE,
                             doc='Enables/disables statistics processing mode',
@@ -103,42 +131,48 @@ class CoherentPEM(Device):
                             fset='write_Statistics_mode')
             self.add_attribute(Statistics_mode)
 
-            Statistics_min = attribute(name='Min',
+            Statistics_min = attribute(name='Statistics_min',
+                            label='Min',
                             dtype='DevDouble',
                             access=AttrWriteType.READ,
                             doc='Queries the min value in batch',
                             fget='read_Statistics_min')
             self.add_attribute(Statistics_min)
 
-            Statistics_max = attribute(name='Max',
+            Statistics_max = attribute(name='Statistics_max',
+                            label='Max',
                             dtype='DevDouble',
                             access=AttrWriteType.READ,
                             doc='Queries the max value in batch',
                             fget='read_Statistics_max')
             self.add_attribute(Statistics_max)
 
-            Statistics_std = attribute(name='Std',
+            Statistics_std = attribute(name='Statistics_std',
+                            label='Std',
                             dtype='DevDouble',
                             access=AttrWriteType.READ,
                             doc='Queries the standard deviation in batch',
                             fget='read_Statistics_std')
             self.add_attribute(Statistics_std)
 
-            Statistics_dose = attribute(name='Dose',
+            Statistics_dose = attribute(name='Statistics_dose',
+                            label='Dose',
                             dtype='DevDouble',
                             access=AttrWriteType.READ,
                             doc='Queries the dose in batch',
                             fget='read_Statistics_dose')
             self.add_attribute(Statistics_dose)
 
-            Statistics_missed = attribute(name='Missed',
+            Statistics_missed = attribute(name='Statistics_missed',
+                            label='Missed',
                             dtype='DevLong',
                             access=AttrWriteType.READ,
                             doc='Queries the number of missed pulses in batch',
                             fget='read_Statistics_missed')
             self.add_attribute(Statistics_missed)
 
-            Statistics_bsize = attribute(name='Statistics Batch Size',
+            Statistics_bsize = attribute(name='Statistics_bsize',
+                            label='Samples',
                             dtype='DevLong',
                             access=AttrWriteType.READ_WRITE,
                             doc='Sets/queries the statistics batch size',
@@ -146,7 +180,8 @@ class CoherentPEM(Device):
                             fset='write_Statistics_bsize')
             self.add_attribute(Statistics_bsize)
 
-            Statistics_rmode = attribute(name='Statistics Restart Mode',
+            Statistics_rmode = attribute(name='Statistics_rmode',
+                            label='Statistics Restart Mode',
                             dtype='DevEnum',
                             enum_labels=["Manual", "Automatic"],
                             access=AttrWriteType.READ_WRITE,
@@ -155,7 +190,8 @@ class CoherentPEM(Device):
                             fset='write_Statistics_rmode')
             self.add_attribute(Statistics_rmode)
 
-            Decimation_rate = attribute(name='Decimation Rate',
+            Decimation_rate = attribute(name='Decimation_rate',
+                            label='Decimation Rate',
                             dtype='DevLong',
                             access=AttrWriteType.READ_WRITE,
                             doc='Sets/queries the pulse decimation rate',
@@ -163,7 +199,8 @@ class CoherentPEM(Device):
                             fset='write_Decimation_rate')
             self.add_attribute(Decimation_rate)
 
-            Aperture_diameter = attribute(name='Aperture Diameter',
+            Aperture_diameter = attribute(name='Aperture_diameter',
+                            label='Aperture Diameter',
                             dtype='DevDouble',
                             access=AttrWriteType.READ_WRITE,
                             unit="mm",
@@ -173,6 +210,7 @@ class CoherentPEM(Device):
             self.add_attribute(Aperture_diameter)
 
             Range = attribute(name='Range',
+                            label='Range',
                             dtype='DevEnum',
                             enum_labels=["High", "Low"],
                             access=AttrWriteType.READ_WRITE,
@@ -184,7 +222,8 @@ class CoherentPEM(Device):
             self.ser.write(bytearray("CONFigure:RANGe:SELect? MAX\r",'ascii'))
             self.maxrange = float(self.ser.readline().decode("utf-8").lstrip().rstrip())
 
-            Trigger_source = attribute(name='Trigger Source',
+            Trigger_source = attribute(name='Trigger_source',
+                            label='Trigger Source',
                             dtype='DevEnum',
                             enum_labels=["Internal", "External"],
                             access=AttrWriteType.READ_WRITE,
@@ -193,7 +232,8 @@ class CoherentPEM(Device):
                             fset='write_Trigger_source')
             self.add_attribute(Trigger_source)
 
-            Trigger_level = attribute(name='Trigger Level',
+            Trigger_level = attribute(name='Trigger_level',
+                            label='Trigger Level',
                             dtype='DevDouble',
                             access=AttrWriteType.READ_WRITE,
                             unit= '%',
@@ -202,7 +242,8 @@ class CoherentPEM(Device):
                             fset='write_Trigger_level')
             self.add_attribute(Trigger_level)
 
-            Trigger_slope = attribute(name='Trigger Slope',
+            Trigger_slope = attribute(name='Trigger_slope',
+                            label='Trigger Slope',
                             dtype='DevEnum',
                             enum_labels=["Positive", "Negative"],
                             access=AttrWriteType.READ_WRITE,
@@ -211,7 +252,8 @@ class CoherentPEM(Device):
                             fset='write_Trigger_slope')
             self.add_attribute(Trigger_slope)
 
-            Trigger_delay = attribute(name='Trigger Delay',
+            Trigger_delay = attribute(name='Trigger_delay',
+                            label='Trigger Delay',
                             dtype='DevDouble',
                             access=AttrWriteType.READ_WRITE,
                             unit= 'us',
@@ -219,6 +261,66 @@ class CoherentPEM(Device):
                             fget='read_Trigger_delay',
                             fset='write_Trigger_delay')
             self.add_attribute(Trigger_delay)
+
+        if 'PowerMax' in self.ID:
+            Sensor_type = attribute(name='Sensor_type',
+                            label='Sensor Type',
+                            dtype='DevString',
+                            access=AttrWriteType.READ,
+                            doc='Queries the sensor type',
+                            fget='read_Sensor_type')
+            self.add_attribute(Sensor_type)
+
+            J_Mode_Trigger_level = attribute(name='J_Mode_Trigger_level',
+                            label='Joule Trigger Level',
+                            dtype='DevEnum',
+                            enum_labels=["LOW", "MEDIUM", "HIGH"],
+                            access=AttrWriteType.READ_WRITE,
+                            doc='Sets/queries the Joule mode trigger level',
+                            fget='read_J_Mode_Trigger_level',
+                            fset='write_J_Mode_Trigger_level')
+            self.add_attribute(J_Mode_Trigger_level)
+
+            Statistics_mean = attribute(name='Statistics_mean',
+                            label='Mean',
+                            dtype='DevDouble',
+                            access=AttrWriteType.READ,
+                            doc='Calculates the rolling average from batch',
+                            fget='read_Statistics_calc_mean')
+            self.add_attribute(Statistics_mean)
+
+            Statistics_std = attribute(name='Statistics_std',
+                            label='Std',
+                            dtype='DevDouble',
+                            access=AttrWriteType.READ,
+                            unit='%',
+                            doc='Calculates the standard deviation from batch',
+                            fget='read_Statistics_calc_std')
+            self.add_attribute(Statistics_std)
+
+            Statistics_min = attribute(name='Statistics_min',
+                            label='Min',
+                            dtype='DevDouble',
+                            access=AttrWriteType.READ,
+                            doc='Calculates the min value from batch',
+                            fget='read_Statistics_calc_min')
+            self.add_attribute(Statistics_min)
+
+            Statistics_max = attribute(name='Statistics_max',
+                            label='Max',
+                            dtype='DevDouble',
+                            access=AttrWriteType.READ,
+                            doc='Calculates the max value from batch',
+                            fget='read_Statistics_calc_max')
+            self.add_attribute(Statistics_max)
+
+            Statistics_bsize = attribute(name='Statistics_bsize',
+                            label='Samples',
+                            dtype='DevLong',
+                            access=AttrWriteType.READ,
+                            doc='Queries the statistics batch size from ring buffer',
+                            fget='read_Statistics_calc_bsize')
+            self.add_attribute(Statistics_bsize)
 
     def delete_device(self):
         self.set_status("The device is in OFF state")
@@ -233,6 +335,12 @@ class CoherentPEM(Device):
 # ------ Read/Write functions ------ #
     def read_Device(self):
         return self.ID
+    
+    def read_ModelName(self):
+        return self.Model
+    
+    def read_SerialNumber(self):
+        return self.SN
     
     def read_Value(self):
         self.ser.write(bytearray("READ?\r",'ascii'))
@@ -259,6 +367,11 @@ class CoherentPEM(Device):
                 self.missed = int(data[5])
                 self.flags = data[6]
                 self.seqid = int(data[7])
+
+        if 'PowerMax' in self.ID:
+            value = float(data[0])
+            self.flags = data[1]
+            self.seqid = int(data[2])
 
         return value * (1000**self.unitscale)
 
@@ -292,6 +405,21 @@ class CoherentPEM(Device):
         value_prop = self.Value.get_properties()
         value_prop.unit = self.unitnames[value]
         self.Value.set_properties(value_prop)
+
+        stat_mean_attr = AttributeProxy(self.get_name()+"/Statistics_mean")
+        stat_mean_prop = stat_mean_attr.get_config()
+        stat_mean_prop.unit = self.unitnames[value]
+        stat_mean_attr.set_config(stat_mean_prop)
+
+        stat_min_attr = AttributeProxy(self.get_name()+"/Statistics_min")
+        stat_min_prop = stat_min_attr.get_config()
+        stat_min_prop.unit = self.unitnames[value]
+        stat_min_attr.set_config(stat_min_prop)
+
+        stat_max_attr = AttributeProxy(self.get_name()+"/Statistics_max")
+        stat_max_prop = stat_max_attr.get_config()
+        stat_max_prop.unit = self.unitnames[value]
+        stat_max_attr.set_config(stat_max_prop)
         
     def read_SeqID(self):
         return self.seqid
@@ -322,6 +450,12 @@ class CoherentPEM(Device):
 
     def write_Gain_factor(self, value):
         self.ser.write(bytearray("CONFigure:GAIN:FACTor "+str(value)+"\r",'ascii'))
+
+    def read_Polling(self):
+        return self.get_attribute_poll_period("Value")
+
+    def write_Polling(self, value):
+        self.poll_attribute("Value", int(value))  
 
     def read_Statistics_mode(self, attr):
         self.ser.write(bytearray("CONFigure:MEASure:STATistics?\r",'ascii'))
@@ -449,6 +583,53 @@ class CoherentPEM(Device):
 
     def write_Trigger_delay(self, attr):
         self.ser.write(bytearray("TRIGger:DELay "+str(attr.get_write_value())+"\r",'ascii'))
+
+    def read_Statistics_calc_mean(self, attr):
+        value_attr_hist = self.value_attr.history(self.poll_buffer)
+        self.value_hist = np.zeros([len(value_attr_hist)])
+
+        for i in range(len(self.value_hist)):
+            self.value_hist[i] = value_attr_hist[i].value
+
+        attr.set_value(np.nanmean(self.value_hist))
+
+    def read_Statistics_calc_std(self, attr):
+        attr.set_value(100*(np.nanstd(self.value_hist)/np.nanmean(self.value_hist)))
+        
+    def read_Statistics_calc_min(self, attr):
+        attr.set_value(np.nanmin(self.value_hist))
+
+    def read_Statistics_calc_max(self, attr):
+        attr.set_value(np.nanmax(self.value_hist))
+
+    def read_Statistics_calc_bsize(self, attr):
+        attr.set_value(self.get_attr_poll_ring_depth("Value"))
+
+    def read_Sensor_type(self, attr):
+        self.ser.write(bytearray("SYSTem:INFormation:TYPE?\r",'ascii'))
+        attr.set_value(self.ser.readline().decode("utf-8").lstrip().rstrip())
+
+    def read_J_Mode_Trigger_level(self, attr):
+        self.ser.write(bytearray("TRIGger:PTJ:LEVel?\r",'ascii'))
+        response = self.ser.readline().decode("utf-8").lstrip().rstrip()
+        if response == "LOW":
+            attr.set_value(0)
+        else:
+            if response == "MEDIUM":
+                attr.set_value(1)
+            else:
+                attr.set_value(2)
+
+    def write_J_Mode_Trigger_level(self, attr):
+        if attr.get_write_value() == 0:
+            set_trigger_source = "LOW"
+        else:
+            if attr.get_write_value() == 1:
+                set_trigger_source = "MEDIUM"
+            else:
+                set_trigger_source = "HIGH"
+
+        self.ser.write(bytearray("TRIGger:PTJ:LEVel "+set_trigger_source+"\r",'ascii'))
 
 # ------ COMMANDS ------ #
 
